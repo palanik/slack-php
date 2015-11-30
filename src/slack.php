6af7632bf4b87f94b0faa628474ea6c2d737155f
palanik <palani.kumanan@gmail.com>
2015-11-29 23:21:03 -0500

    Initial code commit

---
+++ src/slack.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace wrapi;
+
+class slack extends wrapi {
+    public function __construct($token) {
+
+        $opts = array(
+            "headers" => array(
+                "User-Agent" => "slack-wrapi"
+            ),
+            "query" => array(
+                "token" => $token
+            )
+        );
+
+        $json = file_get_contents(__DIR__. '/api/slack.json');
+        $endpoints = json_decode($json, true);
+        parent::__construct('https://slack.com/api/',
+            $endpoints,
+            $opts);
+    }
+
+}
+
+?>
